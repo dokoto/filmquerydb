a4f7ef0d7f611cdc92cbc22b9acb2413b35951cb
administrador <administrador@dione>
2013-12-08 20:49:32 +0100

    update

--- WebContent/js/index.js
+++ WebContent/js/index.js
@@ -1,32 +1,50 @@
-$(document).ready(function() {
+var globs = {
+	eventMounseOver : true
+};
+
+$(document).ready(function()
+{
 	$("#film").attr("disabled", false);
 	$("#search").attr("disabled", false);
 	autoCompleteTitle();
 	$("#panel_filter").buttonset();
 	$("#panel_complete_type").buttonset();
 
+	$("#panel_result").on("mouseenter", "img", function()
+	{
+		if (globs.eventMounseOver)
+		{
+			var id = $(this).attr("id");
+			id = id.substr(id.indexOf("_") + 1);
+			$("#thumb_" + id).hide();
+			$("#play_" + id).show();
+		}
 
-	$("#panel_result").on("mouseenter", "img", function() {
-		var id = $(this).attr("id");
-		id = id.substr(id.indexOf("_")+1);
-		$("#thumb_" + id).hide();
-		$("#play_" + id).show();
-		
-	});	
-	
-	$("#panel_result").on("mouseleave", "img", function() {
-		var id = $(this).attr("id");
-		id = id.substr(id.indexOf("_")+1);
-		$("#play_" + id).hide();
-		$("#thumb_" + id).show();
 	});
-	
-	$("#panel_result").on("click", "img", function() {
+
+	$("#panel_result").on("mouseleave", "img", function()
+	{
+		if (globs.eventMounseOver)
+		{
+			var id = $(this).attr("id");
+			id = id.substr(id.indexOf("_") + 1);
+			$("#play_" + id).hide();
+			$("#thumb_" + id).show();
+		}
+	});
+
+	$("#panel_result").on("click", "img", function()
+	{
 		var id = $(this).attr("id");
+		var idd = id.substr(id.indexOf("_") + 1);
 		id = id.substr(0, id.indexOf("_"));
 		var path = $(this).parent().parent().parent().find("#path").text();
 		if (id == "play")
 		{
+			globs.eventMounseOver = false;
+			$("#thumb_" + idd).hide();
+			$("#play_" + idd).hide();
+			$("#progress_" + idd).show();
 			$.ajax({
 				url : "playFilm.jsp",
 				data : {
@@ -34,27 +52,40 @@ $(document).ready(function() {
 				},
 				type : "POST",
 				dataType : "json",
-				success : function(data) {		
+				success : function(data)
+				{
 					$.fileDownload(data.url, {
-				        preparingMessageHtml: "We are preparing your report, please wait...",
-				        failMessageHtml: "There was a problem generating your report, please try again."
-				    });
+						preparingMessageHtml : "We are preparing your report, please wait...",
+						failMessageHtml : "There was a problem generating your report, please try again."
+					});
+					globs.eventMounseOver = true;
+					$("#thumb_" + idd).show();
+					$("#play_" + idd).hide();
+					$("#progress_" + idd).hide();
 				},
-				error: function(result, status, err) {
-			        log("Error loading data");
-			        return;
-			    }
+				error : function(result, status, err)
+				{
+					globs.eventMounseOver = true;
+					$("#thumb_" + idd).show();
+					$("#play_" + idd).hide();
+					$("#progress_" + idd).hide();
+					log("Error loading data");
+					return;
+				}
 			});
 		}
 	});
-	
-	$("#search").click(function() {
+
+	$("#search").click(function()
+	{
 		$(this).attr("disabled", true);
 		GetResults();
 	});
 
-	$('#film').on('keypress', function(event) {
-		if (event.which == '13') {
+	$('#film').on('keypress', function(event)
+	{
+		if (event.which == '13')
+		{
 			$("#film").attr("disabled", true);
 			$("#search").attr("disabled", true);
 			GetResults();
@@ -62,10 +93,10 @@ $(document).ready(function() {
 		}
 	});
 
-
 });
 
-function GetResults() {
+function GetResults()
+{
 	$.ajax({
 		url : "getFilm.jsp",
 		data : {
@@ -79,7 +110,8 @@ function GetResults() {
 	});
 }
 
-function GetImage(id_name, mongo_id_image) {
+function GetImage(id_name, mongo_id_image)
+{
 	$.ajax({
 		url : "getImage.jsp",
 		data : {
@@ -87,14 +119,16 @@ function GetImage(id_name, mongo_id_image) {
 		},
 		type : "POST",
 		dataType : "json",
-		success : function(data) {		
-			$("#" + id_name).attr("src", "data:image/jpeg;base64," + data.value);				
+		success : function(data)
+		{
+			$("#" + id_name).attr("src", "data:image/jpeg;base64," + data.value);
 		}
 	});
 
 }
 
-function ImageExist(url) {
+function ImageExist(url)
+{
 	var img = new Image();
 	img.src = url;
 	return img.height != 0;
@@ -102,51 +136,41 @@ function ImageExist(url) {
 
 function StripImageName(name)
 {
-	var s1 = name.substr(name.indexOf("/")+1);
+	var s1 = name.substr(name.indexOf("/") + 1);
 	var s2 = s1.substr(0, s1.indexOf("."));
 	return s2;
 }
 
-function buildResultLines(data) {
+function buildResultLines(data)
+{
 	$("#panel_result").html("");
-	for (var i = 0; i < data.values.length; i++) {
+	for (var i = 0; i < data.values.length; i++)
+	{
 		var image_id = StripImageName(data.values[i].foto_mini);
 		$("#panel_result").append(
-				'<div id="line" class="shape wapper">' 
-						+ '<div class="block1">'
-						+ '<img class ="thumb" src="img/ico_film.png" id="thumb_' + image_id + '" alt="thumb_image"/>'
-						+ '<a href="#"><img class="opacy" src="img/video-play.png" id="play_' + image_id + '" alt="play_image"/></a>'
-						+ '</div>'
-						+ '<div class="block2">'
-						+ '<p class="p1">'
-						+ data.values[i].titulo
-						+ '</p>'
-						+ '<p class="p2">'
-						+ data.values[i].release_date + '<br />'
-						+ data.values[i].generos + '<br />'
-						+ data.values[i].directores + '<br />'
-						+ data.values[i].actores + '<br />'
-						+ '<span id="path">' + data.values[i].file_full_path + '</span>'
-						+ '</p>'
-						+ '</div>'
-						+ '<div class="block3">'
-						+ '<p class="p3">'
-						+ data.values[i].sinopsis
-						+ '</p>'
-						+ '</div>'
-						+ '</div>');
-		
+				'<div id="line" class="shape wapper">' + '<div class="block1">' + '<img class ="thumb" src="img/ico_film.png" id="thumb_' + image_id
+						+ '" alt="thumb_image"/>' + '<a href="#"><img class="opacy" src="img/video-play.png" id="play_' + image_id + '" alt="play_image"/></a>'
+						+ '<img class ="progress" src="img/progress.gif" id="progress_' + image_id + '" alt="progress_image"/>' + '</div>'
+						+ '<div class="block2">' + '<p class="p1">' + data.values[i].titulo + '</p>' + '<p class="p2">' + data.values[i].release_date
+						+ '<br />' + data.values[i].generos + '<br />' + data.values[i].directores + '<br />' + data.values[i].actores + '<br />'
+						+ '<span id="path">' + data.values[i].file_full_path + '</span>' + '</p>' + '</div>' + '<div class="block3">' + '<p class="p3">'
+						+ data.values[i].sinopsis + '</p>' + '</div>' + '</div>');
+
 		$("#" + "play_" + image_id).hide();
-		GetImage("thumb_" + image_id, data.values[i].foto_mini);		
-	}	
+		$("#" + "progress_" + image_id).hide();
+		GetImage("thumb_" + image_id, data.values[i].foto_mini);
+	}
 	$("#film").attr("disabled", false);
 	$("#search").attr("disabled", false);
 }
 
-function autoCompleteTitle() {
-	$("#film").keypress(function() {
+function autoCompleteTitle()
+{
+	$("#film").keypress(function()
+	{
 		$("#film").autocomplete({
-			source : function(request, response) {
+			source : function(request, response)
+			{
 				$.ajax({
 					url : "getFilms.jsp",
 					data : {
@@ -156,8 +180,10 @@ function autoCompleteTitle() {
 					},
 					type : "POST",
 					dataType : "json",
-					success : function(data) {
-						response($.map(data.values, function(item) {
+					success : function(data)
+					{
+						response($.map(data.values, function(item)
+						{
 							return {
 								label : item,
 								value : item
@@ -166,20 +192,24 @@ function autoCompleteTitle() {
 					}
 				});
 			},
-			select : function(event, ui) {
+			select : function(event, ui)
+			{
 				if (ui.item)
 					showFilm(ui.item);
 			},
-			open : function() {
+			open : function()
+			{
 				$(this).removeClass("ui-corner-all").addClass("ui-corner-top");
 			},
-			close : function() {
+			close : function()
+			{
 				$(this).removeClass("ui-corner-top").addClass("ui-corner-all");
 			}
 		});
 	});
 }
 
-function showFilm(item) {
+function showFilm(item)
+{
 
 }
--- src/services/FilmPlay.java
+++ src/services/FilmPlay.java
@@ -1,46 +1,186 @@
 package services;
 
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.HashMap;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.codehaus.plexus.util.StringUtils;
+import org.codehaus.plexus.util.cli.CommandLineException;
+import org.codehaus.plexus.util.cli.CommandLineUtils;
+import org.codehaus.plexus.util.cli.Commandline;
+
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class FilmPlay
 {
-	private static final String link = "http://mansierra.homelinux.net:8080/FilmQueryDb/streamLink.m3u";
-	
+	private static final String link = "http://mansierra.homelinux.net:8080/fqdb/streamLink.m3u";
+	private static Thread threadON = null;
+	private static Thread thr_ffServer = null;
+	private static long pid_ffserver = -1;
+	private static Thread thr_ffmpeg = null;
+
 	public static String ManageQueryBy(HttpServletRequest request)
 	{
-		String path = request.getParameter("path");
-		try
+		final String path = request.getParameter("path");
+		try			
 		{
-			Runtime r = Runtime.getRuntime();
-			Process p = r.exec("sffserver \"" + path + "\"");
-			p.waitFor();
-			BufferedReader b = new BufferedReader(new InputStreamReader(p.getInputStream()));
-			String line = "";
-
-			while ((line = b.readLine()) != null)
+			if (pid_ffserver != -1)
+				exe("kill", "/home/administrador", new String[] {"-9", String.valueOf(pid_ffserver)});
+			if (thr_ffServer != null)
 			{
-				System.out.println(line);		
+				if (thr_ffServer.isAlive())
+				{
+					thr_ffServer.interrupt();
+					thr_ffServer = null;
+				}
 			}
-
+			
+			if (thr_ffmpeg != null)
+			{
+				if (thr_ffmpeg.isAlive())
+				{
+					thr_ffmpeg.interrupt();
+					thr_ffmpeg = null;
+				}
+			}
+			
+			thr_ffServer = new Thread()
+			{
+				public void run()
+				{
+					try
+					{
+						pid_ffserver = exe("/opt/bin/ffserver", "/home/administrador", new String[] {"-f", "/etc/ffserverNoDaemon.conf"});
+					} catch (Exception e)
+					{
+						e.printStackTrace();
+					}
+				}
+			};
+			thr_ffServer.start();
+			Thread.sleep(1000);
+			thr_ffmpeg = new Thread()
+			{
+				public void run()
+				{
+					try
+					{
+						exe("/opt/bin/ffmpeg", "/home/administrador", new String[] {"-i", path, "http://localhost:51413/feed1.ffm"});
+					} catch (Exception e)
+					{
+						e.printStackTrace();
+					}
+				}
+			};
+			thr_ffmpeg.start();
+			
+			/*
+			if (threadON != null)
+			{
+				if (threadON.isAlive())
+				{
+					threadON.interrupt();
+					threadON = null;
+				}
+			}
+			threadON = new Thread()
+			{
+				public void run()
+				{
+					try
+					{
+						exe("/usr/local/bin/sffserver", "/home/administrador", new String[] { path });
+					} catch (Exception e)
+					{
+						e.printStackTrace();
+					}
+				}
+			};
+			threadON.start();
+			Thread.sleep(1000);
+			*/
 			Map<String, Object> root = new HashMap<String, Object>();
 			root.put("url", link);
 			ObjectMapper objectMapper = new ObjectMapper();
 			return objectMapper.writeValueAsString(root);
-			
+
 		} catch (Exception e)
 		{
 			e.printStackTrace();
 			return null;
 		}
 
-		
+	}
+
+	private static boolean exeNoWait(String fullCmd) throws IOException, InterruptedException
+	{
+
+		Runtime r = Runtime.getRuntime();
+		Process p = r.exec(fullCmd);
+		BufferedReader b = new BufferedReader(new InputStreamReader(p.getInputStream()));
+		System.out.println(b.readLine());
+
+		return true;
+	}
+
+	private static boolean exe(String fullCmd) throws IOException, InterruptedException
+	{
+
+		Runtime r = Runtime.getRuntime();
+		Process p = r.exec(fullCmd);
+		p.waitFor();
+		BufferedReader b = new BufferedReader(new InputStreamReader(p.getInputStream()));
+		String line = "";
+
+		while ((line = b.readLine()) != null)
+		{
+			System.out.println(line);
+		}
+
+		return true;
+	}
+
+	private static long exe(String command, String workingDir, String[] arguments) throws Exception
+	{
+		Commandline commandline = new Commandline();
+		commandline.setExecutable(command);
+		commandline.addArguments(arguments);
+		commandline.setWorkingDirectory(new File(workingDir));
+
+		CommandLineUtils.StringStreamConsumer err = new CommandLineUtils.StringStreamConsumer();
+		CommandLineUtils.StringStreamConsumer out = new CommandLineUtils.StringStreamConsumer();
+
+		int exitCode;
+		try
+		{
+			exitCode = CommandLineUtils.executeCommandLine(commandline, out, err, 10);
+			
+		} catch (CommandLineException e)
+		{
+			e.printStackTrace();
+			return -1;
+		}
+
+		String output = out.getOutput();
+		if (!StringUtils.isEmpty(output))
+		{
+			System.out.println(output);
+			return -1;
+		}
+
+		String error = err.getOutput();
+		if (!StringUtils.isEmpty(error))
+		{
+			System.out.println(error);
+			return -1;
+		}
+
+		return commandline.getPid();
 	}
 
 }
